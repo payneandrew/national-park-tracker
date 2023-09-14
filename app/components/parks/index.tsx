ad29da2708e3d13cb47f4f9a72db5faea5edcadb
Andrew Payne <46719251+payneandrew@users.noreply.github.com>
2023-09-14 15:05:11 -0400

    Checks and unchecked

--- app/components/parks/index.tsx
+++ app/components/parks/index.tsx
@@ -7,13 +7,45 @@ interface ParksProps {
 }
 
 const Parks: React.FC<ParksProps> = ({ parks }) => {
+  if (!localStorage.getItem("visited")) {
+    localStorage.setItem("visited", JSON.stringify([]));
+  }
+
+  const visited = JSON.parse(localStorage.getItem("visited")!);
+
+  const isParkVisited = (parkId: string) => {
+    return visited.includes(parkId);
+  };
+
+  const setVisited = (parkId: string) => {
+    localStorage.setItem("visited", JSON.stringify([...visited, parkId]));
+  };
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {parks.map((park) => (
         <div key={park.id} className="bg-white p-4 rounded-lg shadow-md">
-          <h2 className="text-xl font-semibold mb-2 text-rocks-canyons">
-            {park.fullName}
-          </h2>
+          <div className="flex justify-between items-start">
+            <h2 className="text-xl font-semibold mb-2 text-rocks-canyons">
+              {park.fullName}
+            </h2>
+            <button
+              className="flex-shrink-0"
+              onClick={() => setVisited(park.id)}
+            >
+              <Image
+                src={
+                  isParkVisited(park.id)
+                    ? "/icons/checked.png"
+                    : "/icons/unchecked.png"
+                }
+                alt={isParkVisited(park.id) ? "Visited" : "Not Visited"}
+                width={50}
+                height={50}
+              />
+            </button>
+          </div>
+
           <p className="text-gray-700">{park.description}</p>
           {park.images[0] && (
             <Image
