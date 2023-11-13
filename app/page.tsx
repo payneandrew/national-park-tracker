844b2c9cc69484e381d05cfac7d3b1053c8255e5
Andrew Payne <46719251+payneandrew@users.noreply.github.com>
2023-11-13 09:38:18 -0500

    Change enum

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { USStates } from '@/mocks/states';
 import Link from 'next/link';
 
 export default function Page() {
-  const states = Object.entries(USStates).map(([name, abbreviation]) => ({
-    name,
-    abbreviation,
+  const states = Object.entries(USStates).map(([stateCode, stateName]) => ({
+    stateCode,
+    stateName,
   }));
   return (
     <>
@@ -13,14 +13,14 @@ export default function Page() {
       </h1>
       <main className="flex min-h-screen flex-col justify-between p-12">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {states.map(({ name, abbreviation }) => (
+          {states.map(({ stateCode, stateName }) => (
             <Link
-              key={abbreviation}
-              href={{ pathname: `/state/${abbreviation}` }}
+              key={stateCode}
+              href={{ pathname: `/state/${stateCode}` }}
               className="rounded overflow-hidden shadow-lg p-4 bg-wood-trees cursor-pointer transform transition-transform duration-200 hover:scale-105"
             >
               <p className="font-bold text-xl mb-2 text-white text-bold">
-                {name}
+                {stateName}
               </p>
             </Link>
           ))}
--- app/state/[stateCode]/page.tsx
+++ app/state/[stateCode]/page.tsx
@@ -1,10 +1,11 @@
+import { USStates } from '@/mocks/states';
 import axios from 'axios';
 import Head from 'next/head';
 import Parks from '../../components/parks';
 export const dynamic = 'force-dynamic';
 
-const StatePage = async ({ params }: { params: { abbreviation: string } }) => {
-  const stateCode = params.abbreviation;
+const StatePage = async ({ params }: { params: { stateCode: string } }) => {
+  const stateCode = params.stateCode;
 
   const { data: nationalParks } = await axios.get(
     'https://developer.nps.gov/api/v1/parks',
@@ -22,7 +23,7 @@ const StatePage = async ({ params }: { params: { abbreviation: string } }) => {
   return (
     <div>
       <Head>
-        <title>{`${state} Parks`}</title>
+        <title>{`${stateCode} Parks`}</title>
         <meta
           name="description"
           content="Explore national parks and plan your visits."
@@ -30,7 +31,9 @@ const StatePage = async ({ params }: { params: { abbreviation: string } }) => {
       </Head>
 
       <div className="container mx-auto mt-8">
-        <h1 className="text-3xl font-semibold mb-4 text-white">{`${state} Parks`}</h1>
+        <h1 className="text-3xl font-semibold mb-4 text-white">{`${
+          USStates[stateCode as keyof typeof USStates]
+        } Parks`}</h1>
         {nationalParks && <Parks parks={nationalParks.data} />}
       </div>
     </div>
