18d5532bc55e18b86602bf7c70c28e45efc2e326
Andrew Payne <andrewpayne64@gmail.com>
2024-02-01 13:30:31 -0500

    Fix visited parks

--- app/components/visited-parks/index.tsx
+++ app/components/visited-parks/index.tsx
@@ -19,6 +19,8 @@ const VisitedParks: React.FC = () => {
   };
 
   const storageVisitedParksCodes = getStoredVisitedParkCodes();
+
+  console.log('storageVisitedParksCodes', storageVisitedParksCodes);
   const { data, error, isLoading } = useSWR<ParkResponse>(
     `https://developer.nps.gov/api/v1/parks?api_key=${
       process.env.NEXT_PUBLIC_NP_API_KEY
@@ -28,32 +30,34 @@ const VisitedParks: React.FC = () => {
     fetcher
   );
 
-  const visitedParks = data?.data || [];
+  const visitedParks = storageVisitedParksCodes ? data?.data : [];
 
   return (
     <div className="flex items-center justify-center">
-      {!isLoading ? (
+      {!isLoading && visitedParks ? (
         <>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {visitedParks.map((park) => (
-              <Link key={park.id} href={`/park-detail/${park.parkCode}`}>
-                <div className="bg-white p-4 rounded-lg shadow-md">
-                  <div className="flex justify-between items-start">
-                    <h2 className="text-xl font-semibold mb-2 text-rocks-canyons">
-                      {park.fullName}
-                    </h2>
-                    <Image
-                      src="/icons/checked.png"
-                      alt="Visited"
-                      width={50}
-                      height={50}
-                    />
-                  </div>
+              <Link
+                className="bg-white p-4 rounded-lg shadow-md"
+                key={park.id}
+                href={`/park-detail/${park.parkCode}`}
+              >
+                <div className="flex justify-between items-start">
+                  <h2 className="text-xl font-semibold mb-2 text-rocks-canyons">
+                    {park.fullName}
+                  </h2>
+                  <Image
+                    src="/icons/checked.png"
+                    alt="Visited"
+                    width={50}
+                    height={50}
+                  />
                 </div>
               </Link>
             ))}
           </div>
-          {visitedParks.length <= 0 && (
+          {visitedParks.length === 0 && (
             <div>You have not added any parks yet.</div>
           )}
           {error && <div>There was an error loading the parks.</div>}
