15fdb9bc5c7df096af5e6a5593097539f34b70e8
Andrew Payne <andrewpayne64@gmail.com>
2024-02-13 12:10:57 -0500

    Mutate data

--- app/hooks/use-visited-parks/index.ts
+++ app/hooks/use-visited-parks/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import useSWR from 'swr';
 
 const useVisitedParks = () => {
-  const { data: parkCodes } = useSWR<string[]>(
+  const { data: parkCodes, mutate } = useSWR<string[]>(
     `${process.env.NEXT_PUBLIC_API_URL}/visited-parks/park-codes`,
     fetcher
   );
@@ -18,24 +18,28 @@ const useVisitedParks = () => {
   ) => {
     event.stopPropagation();
     event.preventDefault();
-
-    isParkVisited(parkCode) === true
-      ? await axios.delete(
-          `${process.env.NEXT_PUBLIC_API_URL}/visited-parks/${parkCode}`,
-          {
-            headers: {
-              Accept: 'application/json',
-            },
-          }
-        )
-      : await axios.post(
-          `${process.env.NEXT_PUBLIC_API_URL}/visited-parks/${parkCode}`,
-          {
-            headers: {
-              Accept: 'application/json',
-            },
-          }
-        );
+    try {
+      isParkVisited(parkCode) === true
+        ? await axios.delete(
+            `${process.env.NEXT_PUBLIC_API_URL}/visited-parks/${parkCode}`,
+            {
+              headers: {
+                Accept: 'application/json',
+              },
+            }
+          )
+        : await axios.post(
+            `${process.env.NEXT_PUBLIC_API_URL}/visited-parks/${parkCode}`,
+            {
+              headers: {
+                Accept: 'application/json',
+              },
+            }
+          );
+      mutate();
+    } catch (error) {
+      console.error('Error toggling visited park:', error);
+    }
   };
 
   return { isParkVisited, toggleVisited };
