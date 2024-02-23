3a4d9291fb224f39b27d0770f9911c33439e32c1
Andrew Payne <andrewpayne64@gmail.com>
2024-02-23 13:45:38 -0500

    redesign landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 'use client';
 
 import Link from 'next/link';
+import ImageTile from './components/image-tile';
+import Loading from './components/loading';
 import MapContainerClustering from './components/map-container-clustering';
 import SquareContainer from './components/square-container';
 import { useAllParks } from './hooks/use-all-parks';
 
 export default function Page() {
-  const { data: parks } = useAllParks();
-  const randomPark =
-    parks?.data[Math.floor(Math.random() * (Number(parks?.total) || 0))];
+  const { data: parks, isLoading } = useAllParks();
 
-  const backgroundImageStyle = randomPark?.images
-    ? {
-        backgroundImage: `url(${randomPark.images[0].url})`,
-      }
-    : { backgroundColor: 'white' };
+  const getRandomPark = () => {
+    return parks?.data[Math.floor(Math.random() * (Number(parks?.total) || 0))];
+  };
+
+  const randomParks = Array.from({ length: 4 }, () => getRandomPark());
 
   const markerPositions = parks?.data.map((park) => ({
     lat: Number(park.latitude),
@@ -57,10 +57,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
-          {/* <SquareContainer
-            src="/images/rafting.jpeg"
-            backgroundColor="black-leather-jacket"
-          /> */}
           <SquareContainer
             src="/images/walking-in-the-woods.jpeg"
             backgroundColor="coffee-brown"
@@ -73,33 +69,26 @@ export default function Page() {
             src="/images/sunset.jpeg"
             backgroundColor="coffee-brown"
           />
-          {/* <SquareContainer
-            src="/images/rafting.jpeg"
-            backgroundColor="black-leather-jacket"
-          /> */}
         </div>
       </div>
-      <div className="flex justify-start flex-col pl-20 gap-6">
+      <div className="flex justify-start flex-col gap-6">
         <h2 className="text-3xl text-copper-brown mb-4 font-abril-fatface">
           Dive into a park
         </h2>
-        {randomPark && (
-          <Link
-            className="pt-2 px-2 rounded-lg shadow-md relative overflow-hidden"
-            key={randomPark.id}
-            href={`/park-detail/${randomPark.parkCode}`}
-            style={{
-              ...backgroundImageStyle,
-              backgroundSize: 'cover',
-              width: '22rem',
-              height: '12rem',
-            }}
-          >
-            <div className="absolute inset-0 bg-black opacity-40 hover:opacity-0 transition-opacity"></div>
-            <div className="text-lg font-bold text-white absolute top-4 left-4 z-10">
-              <h2> {randomPark.fullName}</h2>
-            </div>
-          </Link>
+        {!isLoading && randomParks ? (
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-3">
+            {randomParks.map((randomPark) => {
+              return randomPark ? (
+                <ImageTile key={randomPark.id} park={randomPark} />
+              ) : (
+                <></>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="flex items-center justify-center">
+            <Loading />
+          </div>
         )}
         {parks && markerPositions && (
           <MapContainerClustering markerPositions={markerPositions} zoom={4} />
